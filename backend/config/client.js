e225fb4b070387be606a39ade6ee9127405b8ca7
Jiu Choe <76275664+jiuchoe4@users.noreply.github.com>
2021-05-26 21:22:15 -0700

    Update client.js

--- backend/config/client.js
+++ backend/config/client.js
@@ -1,4 +1,4 @@
 module.exports = {
     clientId : "tXvUnqjBYFjv4pmVVkJ3Eg==",
-    clientKey : "iSAD18xyXqHacJslOugKCRSgFA0W9MzgLLVQjiHvFZgOtgu81Ld4T_6Bdvt3AJc46a-QA6vxcQY_D4ZoPSVXXQ==",
+    clientKey : "",
     bearerToken: ""
